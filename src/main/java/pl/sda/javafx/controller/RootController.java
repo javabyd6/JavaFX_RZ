e4678dec900ac54671f8843133f0210d9677510c
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-03-10 16:02:51 +0100

    TODO FX

--- src/main/java/pl/sda/javafx/controller/RootController.java
+++ src/main/java/pl/sda/javafx/controller/RootController.java
@@ -1,5 +1,6 @@
 package pl.sda.javafx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,12 +18,20 @@ public class RootController implements Initializable {
     @FXML
     private Button search;
 
-
+    @FXML
     private TextField city;
 
+    @FXML
     private Label TemperatureIn;
 
     public void initialize(URL location, ResourceBundle resources) {
 
+        city.setText("TEST123");
+        TemperatureIn.setText("");
+
+    }
+
+    public void setCity() {
+        System.out.println("KLIK KLIK KLIK");
     }
 }
